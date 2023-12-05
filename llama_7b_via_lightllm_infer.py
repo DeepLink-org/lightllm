25623df3976a600b5472006a79e354d9c3237df1
POI-WX <wangxing2@sensetime.com>
2023-12-05 21:33:37 +0800

    set timeout for waiting the server to start

--- llama_7b_via_lightllm_infer.py
+++ llama_7b_via_lightllm_infer.py
@@ -3,6 +3,16 @@ import os
 import subprocess
 import time
 import argparse
+import socket
+
+
+def check_server(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((host, port))
+            return True
+        except ConnectionRefusedError:
+            return False
 
 
 def start_server(model_dir, tokenizer_mode, tp, max_total_token_num):
@@ -32,6 +42,10 @@ if __name__ == '__main__':
                         help="Model tp parallel size, the default is 1")
     parser.add_argument("--max_total_token_num", type=int, default=6000,
                         help="The total token nums the gpu and model can support")
+    parser.add_argument("--host", type=str, default="127.0.0.1",
+                        help="Host where the server will be running")
+    parser.add_argument("--port", type=int, default=8000,
+                        help="Port on which the server will be listening")
     args = parser.parse_args()
 
     current_working_directory = os.path.dirname(os.path.abspath(__file__))
@@ -40,8 +54,20 @@ if __name__ == '__main__':
         target=start_server, args=(args.model_dir, args.tokenizer_mode, args.tp, args.max_total_token_num))
     server_process.start()
 
-    # 等待服务器启动
-    time.sleep(180)
+    # 设置超时时间为10分钟
+    timeout = 10 * 60
+    start_time = time.time()
+
+    # 动态等待服务器启动
+    print("等待服务器启动...")
+    while not check_server(args.host, args.port):
+        if time.time() - start_time > timeout:
+            print("等待服务器启动超时，程序退出。")
+            server_process.terminate()
+            server_process.join()
+            exit(1)
+        time.sleep(5)
+    print("服务器已启动!")
 
     # 启动客户端进程
     client_process = multiprocessing.Process(
