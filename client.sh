e509538f01148afb91c0496e9880c02c384b99fc
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-04-19 07:33:11 +0000

    modify client.sh.

--- client.sh
+++ client.sh
@@ -1,9 +1,17 @@
+unset ASCEND_GLOBAL_LOG_LEVEL
+# export ASCEND_GLOBAL_EVENT_ENABLE=0
+
 export DIPU_DUMP_OP_ARGS=-1
-export DIPU_MOCK_CUDA=True
+export DIPU_MOCK_CUDA=True 
 
 export DIPU_KEEP_TORCHOP_DEFAULT_IMPL_OPS="rsqrt.out,mm,linear,_softmax.out"
 
-curl http://0.0.0.0:8888/generate \
--X POST \
--d '{"inputs":"Hello?", "parameters":{"max_new_tokens":30, "frequency_penalty":1}}' \
--H 'Content-Type: application/json'
+curl http://0.0.0.0:8899/generate     \
+    -X POST                           \
+    -d '{"inputs":"How are you? ","parameters":{"max_new_tokens":20, "frequency_penalty":1}}' \
+    -H 'Content-Type: application/json'
+
+#  curl http://localhost:8888/generate   \
+#     -X POST                           \
+#     -d '{"inputs":"What is the result of one plus two?","parameters":{"max_new_tokens":20, "frequency_penalty":1}}' \
+#     -H 'Content-Type: application/json'
