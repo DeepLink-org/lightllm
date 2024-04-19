638f0986aed00d225b9024707ef175b3d34617aa
zhoushenglong <zhoushenglong@pjlab.org.cn>
2024-04-19 07:35:38 +0000

    fix apply_penalty.

--- lightllm/common/basemodel/triton_kernel/apply_penalty.py
+++ lightllm/common/basemodel/triton_kernel/apply_penalty.py
@@ -52,18 +52,40 @@ import torch
 #     )
 #     return
 
+# @torch.no_grad()
+# def apply_penalty_torch(Logits, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids, 
+#                         p_token_counts, p_cumsum_seq_len, p_max_len_in_batch):
+#     batch = Logits.size(0)
+#     for i in range(batch):
+#         cur_batch_start_index = p_cumsum_seq_len[i]
+#         cur_batch_end_index = p_cumsum_seq_len[i+1]
+#         slice = torch.arange(cur_batch_start_index, cur_batch_end_index, dtype=torch.int64, 
+#                              layout = Logits.layout, device = torch.device('cpu'))
+#         cur_token_ids = p_token_ids[slice]
+#         cur_token_counts = p_token_counts[slice]
+#         cur_logits = Logits[i].index_select(0, cur_token_ids)
+#         rep_logits = torch.where(cur_logits > 0, cur_logits / repetition_penalty[i], cur_logits * repetition_penalty[i])
+#         rep_logits = rep_logits - cur_token_counts * freqency_penalty[i] - presence_penalty[i]
+#         Logits[i, cur_token_ids] = rep_logits
+
 @torch.no_grad()
 def apply_penalty_torch(Logits, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids, 
                         p_token_counts, p_cumsum_seq_len, p_max_len_in_batch):
-    batch = Logits.size(0)
-    for i in range(batch):
-        cur_batch_start_index = p_cumsum_seq_len[i]
-        cur_batch_end_index = p_cumsum_seq_len[i+1]
-        slice = torch.arange(cur_batch_start_index, cur_batch_end_index, dtype=torch.int64, 
-                             layout = Logits.layout, device = torch.device('cpu'))
-        cur_token_ids = p_token_ids[slice]
-        cur_token_counts = p_token_counts[slice]
-        cur_logits = Logits[i].index_select(0, cur_token_ids)
-        rep_logits = torch.where(cur_logits > 0, cur_logits / repetition_penalty[i], cur_logits * repetition_penalty[i])
-        rep_logits = rep_logits - cur_token_counts * freqency_penalty[i] - presence_penalty[i]
-        Logits[i, cur_token_ids] = rep_logits
+    cur_batch_start_index = p_cumsum_seq_len[0]
+    cur_batch_end_index = p_cumsum_seq_len[1]
+    slice = torch.arange(cur_batch_start_index, cur_batch_end_index, dtype=torch.int64,
+                         layout = Logits.layout, device = Logits.device)
+    return apply(Logits, slice, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids, p_token_counts)
+
+
+@torch.no_grad()
+def apply(Logits, slice, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids, p_token_counts):
+    cur_token_ids = p_token_ids[slice]
+    cur_token_counts = p_token_counts[slice]
+    cur_logits = Logits[0].index_select(0, cur_token_ids)
+    rep_logits = torch.where(cur_logits > 0, cur_logits / repetition_penalty[0], cur_logits * repetition_penalty[0])
+
+    rep_logits = rep_logits - cur_token_counts * freqency_penalty[0] - presence_penalty[0]
+    Logits[0, cur_token_ids] = rep_logits
+
+    return Logits
