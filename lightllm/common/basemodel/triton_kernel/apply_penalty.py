21f5b53608252ba26c0a540619827f9e77e03078
zhaochaoxing <simonzhao166@gmail.com>
2024-05-11 08:07:16 +0000

    use diopi apply_penalty

--- lightllm/common/basemodel/triton_kernel/apply_penalty.py
+++ lightllm/common/basemodel/triton_kernel/apply_penalty.py
@@ -1,17 +1,18 @@
 import torch
 
 @torch.no_grad()
-def apply_penalty(Logits, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids,
+def apply_penalty_v2(Logits, presence_penalty, freqency_penalty, repetition_penalty, p_token_ids,
                         p_token_counts, p_cumsum_seq_len, p_max_len_in_batch):
-    batch = Logits.size(0)
-    for i in range(batch):
-        cur_batch_start_index = p_cumsum_seq_len[i]
-        cur_batch_end_index = p_cumsum_seq_len[i+1]
-        slice = torch.arange(cur_batch_start_index, cur_batch_end_index, dtype=torch.int64,
-                             layout = Logits.layout, device = torch.device('cpu'))
-        cur_token_ids = p_token_ids[slice]
-        cur_token_counts = p_token_counts[slice]
-        cur_logits = Logits[i].index_select(0, cur_token_ids)
-        rep_logits = torch.where(cur_logits > 0, cur_logits / repetition_penalty[i], cur_logits * repetition_penalty[i])
-        rep_logits = rep_logits - cur_token_counts * freqency_penalty[i] - presence_penalty[i]
-        Logits[i, cur_token_ids] = rep_logits
+    pass
+    # batch = Logits.size(0)
+    # for i in range(batch):
+    #     cur_batch_start_index = p_cumsum_seq_len[i]
+    #     cur_batch_end_index = p_cumsum_seq_len[i+1]
+    #     slice = torch.arange(cur_batch_start_index, cur_batch_end_index, dtype=torch.int64,
+    #                          layout = Logits.layout, device = torch.device('cpu'))
+    #     cur_token_ids = p_token_ids[slice]
+    #     cur_token_counts = p_token_counts[slice]
+    #     cur_logits = Logits[i].index_select(0, cur_token_ids)
+    #     rep_logits = torch.where(cur_logits > 0, cur_logits / repetition_penalty[i], cur_logits * repetition_penalty[i])
+    #     rep_logits = rep_logits - cur_token_counts * freqency_penalty[i] - presence_penalty[i]
+    #     Logits[i, cur_token_ids] = rep_logits
