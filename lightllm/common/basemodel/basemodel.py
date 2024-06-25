fix paged_attention precision and clean code.

lightllm/common/basemodel/basemodel.py
@@ -63,7 +63,6 @@ class TpPartBaseModel:
 
         self.opt_context_forward = torch.compile(self._context_forward, backend='ascendgraph', dynamic=self.dynamic)
         self.opt_token_forward = torch.compile(self._token_forward, backend='ascendgraph', dynamic=self.dynamic)
-        self.opt_all_layer_token_forward_with_pre_post_process = torch.compile(self.all_layer_token_forward_with_pre_post_process, backend='ascendgraph', dynamic=self.dynamic)
 
         return
     
@@ -154,8 +153,6 @@ class TpPartBaseModel:
             total_token_num,
             max_len_in_batch,
             input_ids : torch.Tensor,
-            masks : torch.Tensor,
-            is_padding : bool,
             b_req_idx : torch.Tensor,
             b_start_loc : torch.Tensor,
             b_seq_len : torch.Tensor,
@@ -167,13 +164,13 @@ class TpPartBaseModel:
         infer_state.layer0_weight = self.trans_layers_weight[0]
         infer_state.embed_dim = self.embed_dim_
         if is_prefill:
-            predict_logics = self._prefill(infer_state, batch_size, total_token_num, max_len_in_batch, input_ids, masks, is_padding, b_req_idx, b_start_loc, b_seq_len, multimodal_params)
+            predict_logics = self._prefill(infer_state, batch_size, total_token_num, max_len_in_batch, input_ids, b_req_idx, b_start_loc, b_seq_len, multimodal_params)
             return predict_logics
         else:
-            return self._decode(infer_state, batch_size, total_token_num, max_len_in_batch, input_ids, masks, is_padding, b_req_idx, b_start_loc, b_seq_len, multimodal_params)
+            return self._decode(infer_state, batch_size, total_token_num, max_len_in_batch, input_ids, b_req_idx, b_start_loc, b_seq_len, multimodal_params)
 
     @record_function('_prefill')    
-    def _prefill(self, infer_state:InferStateInfo, batch_size, total_token_num, max_len_in_batch, input_ids, masks, is_padding, b_req_idx, b_start_loc, b_seq_len, multimodal_params):
+    def _prefill(self, infer_state:InferStateInfo, batch_size, total_token_num, max_len_in_batch, input_ids, b_req_idx, b_start_loc, b_seq_len, multimodal_params):
         infer_state.is_prefill = True
         infer_state.return_all_prompt_logprobs = self.return_all_prompt_logprobs
         infer_state.batch_size = batch_size
@@ -193,7 +190,8 @@ class TpPartBaseModel:
         infer_state.mem_is_contiguous = False
         infer_state.key_buffer = torch.empty((batch_size * max_len_in_batch, self.tp_k_head_num_* self.head_dim_), dtype=torch.float16, device="cuda")
         infer_state.value_buffer = torch.empty((batch_size * max_len_in_batch, self.tp_v_head_num_* self.head_dim_), dtype=torch.float16, device="cuda")
-        infer_state.init_some_extra_state(self, input_ids, masks, is_padding)
+
+        infer_state.init_some_extra_state(self, input_ids)
         infer_state.block_indices = torch.empty((total_token_num,), dtype = torch.int32, device='cuda')
         infer_state.kv_start_indices = torch.empty((total_token_num,), dtype = torch.int32, device='cuda')
         kv_start = 0
@@ -219,12 +217,7 @@ class TpPartBaseModel:
         return predict_logics
     
     @record_function('_decode')
-    def _decode(self, infer_state:InferStateInfo, batch_size, total_token_num, max_len_in_batch, input_ids, masks, is_padding, b_req_idx, b_start_loc, b_seq_len, multimodal_params):
-        print('####: block_size: ', PagingRequestManager.BLOCK_SIZE)
-        print('####: q_head_num: ', self.tp_q_head_num_)
-        print('####: kv_head_num: ', self.tp_k_head_num_)
-        print('####: dim: ', self.head_dim_)
-        print('####: o_head_num: ', self.layers_infer[0].tp_o_head_num_)
+    def _decode(self, infer_state:InferStateInfo, batch_size, total_token_num, max_len_in_batch, input_ids, b_req_idx, b_start_loc, b_seq_len, multimodal_params):
         infer_state.is_prefill = False
         infer_state.batch_size = batch_size
         infer_state.total_token_num = total_token_num
@@ -244,8 +237,8 @@ class TpPartBaseModel:
         infer_state.mem_is_contiguous = False
         infer_state.key_buffer = torch.empty((batch_size, self.tp_k_head_num_* self.head_dim_), dtype=torch.float16, device="cuda")
         infer_state.value_buffer = torch.empty((batch_size, self.tp_v_head_num_* self.head_dim_), dtype=torch.float16, device="cuda")
-        # infer_state.mem_index = self.req_manager.mem_index_offset[:batch_size] + b_seq_len - 1
-        infer_state.init_some_extra_state(self, input_ids, masks, is_padding)
+
+        infer_state.init_some_extra_state(self, input_ids)
         infer_state.block_table = self.req_manager.get_batched_block_table(b_req_idx)
         infer_state.block_indices = torch.empty((batch_size,), dtype = torch.int32, device='cuda')
         for b_idx in range(batch_size):
@@ -258,7 +251,6 @@ class TpPartBaseModel:
         infer_state.kv_start_indices = infer_state.block_indices * PagingRequestManager.BLOCK_SIZE + last_block_offsets
         infer_state.int_index_list = infer_state.kv_start_indices.tolist()
         infer_state.int_index_list_t = [x + 1 for x in infer_state.int_index_list]
-        
         
         default_pg = None
         if self.world_size_ > 1:
@@ -346,29 +338,26 @@ class TpPartBaseModel:
         input_embs = self.pre_infer.context_forward(input_ids, infer_state, self.pre_post_weight, default_pg)
         # for i in range(self.layers_num):
         for i in range(1):
-            input_embs = self.layers_infer[i].full_context_attention(input_embs, infer_state, self.trans_layers_weight[i], default_pg)
+            input_embs = self.layers_infer[i].opt_full_context_attention(input_embs, infer_state, self.trans_layers_weight[i], default_pg)
 
         predict_logics = self.post_infer.token_forward(input_embs, infer_state, self.pre_post_weight, default_pg, return_logics=True)
         
         return predict_logics
     
-    def all_layer_token_forward_with_pre_post_process(self, cuda_input_ids, infer_state, current_len, max_seq_length, batch_size, default_pg):
-        input_embs = self.pre_infer.token_forward(cuda_input_ids, infer_state, self.pre_post_weight, default_pg)
-        # for i in range(self.layers_num):
-        for i in range(1):
-            input_embs = self.layers_infer[i].opt_full_token_attention(input_embs, infer_state, self.trans_layers_weight[i], current_len, max_seq_length, default_pg)
-        predict_logics = self.post_infer.dicp_token_forward(input_embs, infer_state, self.pre_post_weight, batch_size, default_pg, return_logics=True)
-        
-        return predict_logics
-
     @record_function('_token_forward')
     @final
     def _token_forward(self, input_ids, infer_state: InferStateInfo, default_pg):
-        with record_function('token_forward_with_pre_post_process'):
-            batch_size = infer_state.batch_size
-            cuda_input_ids = input_ids
-            predict_logics = self.all_layer_token_forward_with_pre_post_process(cuda_input_ids, infer_state, infer_state.int_index_list[0], self.max_seq_length, batch_size, default_pg)
+        batch_size = infer_state.batch_size
+        cuda_input_ids = input_ids
 
+        input_embs = self.pre_infer.token_forward(cuda_input_ids, infer_state, self.pre_post_weight, default_pg)
+
+        # for i in range(self.layers_num):
+        for i in range(1):
+            input_embs = self.layers_infer[i].opt_full_token_attention(input_embs, infer_state, self.trans_layers_weight[i], self.max_seq_length, default_pg)
+
+        predict_logics = self.post_infer.dicp_token_forward(input_embs, infer_state, self.pre_post_weight, batch_size, default_pg, return_logics=True)
+        
         return predict_logics
     
     @final

lightllm/models/llama/layer_infer/transformer_layer_infer.py
@@ -40,7 +40,7 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         self.opt_context_post_process = torch.compile(self.context_post_process, backend='ascendgraph', dynamic=dynamic_compiler)
         self.opt_token_pre_process = torch.compile(self.token_pre_process, backend='ascendgraph', dynamic=dynamic_compiler)
         self.opt_token_post_process = torch.compile(self.token_post_process, backend='ascendgraph', dynamic=dynamic_compiler)
-        self.opt_ascend_prompt_attention_inference = torch.compile(self.ascend_prompt_attention_kernel, backend='ascendgraph', dynamic=dynamic_compiler)
+        self.opt_ascend_prompt_attention_kernel = torch.compile(self.ascend_prompt_attention_kernel, backend='ascendgraph', dynamic=dynamic_compiler)
         self.opt_ascend_incre_attention_kernel = torch.compile(self.ascend_incre_attention_kernel, backend='ascendgraph', dynamic=dynamic_compiler)
         self.opt_full_context_attention= torch.compile(self.full_context_attention, backend='ascendgraph', dynamic=dynamic_compiler)
         self.opt_full_token_attention= torch.compile(self.full_token_attention, backend='ascendgraph', dynamic=dynamic_compiler)
@@ -78,9 +78,7 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
                               o_tensor1.view(-1, self.tp_q_head_num_, self.head_dim_),
                               infer_state.b_start_loc,
                               infer_state.b_seq_len,
-                              infer_state.max_len_in_batch,
-                              infer_state.masks,
-                              infer_state.is_padding)
+                              infer_state.max_len_in_batch)
         return o_tensor
     
     @record_function('transformer_token_decode_attention_normal')
@@ -105,8 +103,6 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         o_tensor = token_softmax_reducev_fwd(att_m_tensor, 
                                 infer_state.mem_manager.value_buffer[self.layer_num_],
                                 o_tensor1.view(calcu_shape1),
-                                infer_state.is_padding,
-                                infer_state.masks,
                                 infer_state.req_manager.req_to_token_indexs,
                                 infer_state.b_req_idx,
                                 infer_state.b_start_loc,
@@ -119,8 +115,8 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         out = torch.ops.lightllm.rotary_emb.default(x, cos, sin)
         return out
     
-    def ascend_prompt_attention_kernel(self, q, k, v, seqlen, num_head , head_dim):
-        return torch.ops.lightllm.prompt_attention_inference.default(q, k, v, seqlen, num_head, head_dim)
+    def ascend_prompt_attention_kernel(self, q, k, v, seqlen, num_head, head_dim, num_key_value_heads):
+        return torch.ops.lightllm.prompt_attention_inference.default(q, k, v, seqlen, num_head, head_dim, num_key_value_heads)
     
     def ascend_incre_attention_kernel(self, q, k, v, int_index_list, max_seq_length):
         return torch.ops.lightllm.flash_attention_inference.default(q, k, v, int_index_list, max_seq_length)
@@ -143,9 +139,8 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         cache_v = torch.mm(input1.view(-1, self.embed_dim_), layer_weight.v_weight_).view(-1, self.tp_v_head_num_, self.head_dim_)
 
         # post cache_kv
-        infer_state.req_manager.fill_kv_cache(self.layer_num_, cache_k, cache_v, infer_state)
-        # infer_state.mem_manager.key_buffer[self.layer_num_][infer_state.mem_start:infer_state.mem_end] = cache_k
-        # infer_state.mem_manager.value_buffer[self.layer_num_][infer_state.mem_start:infer_state.mem_end] = cache_v
+        infer_state.mem_manager.key_buffer[self.layer_num_][infer_state.block_indices] = cache_k
+        infer_state.mem_manager.value_buffer[self.layer_num_][infer_state.block_indices] = cache_v
 
         return q, cache_k, cache_v
 
@@ -179,18 +174,16 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
     def full_context_attention(self, input_embding, infer_state: LlamaInferStateInfo, layer_weight, default_pg):
         q, k, v = self.opt_context_pre_process(input_embding, infer_state, layer_weight)
 
-        q = q.view(-1, self.tp_q_head_num_, self.head_dim_)
-
         batch, num_head, head_dim = infer_state.b_start_loc.shape[0], self.tp_q_head_num_, self.head_dim_
         seqlen = infer_state.b_seq_len
+        num_key_value_heads = self.tp_k_head_num_
 
-        out = self.opt_ascend_prompt_attention_inference(q.view(batch, -1, num_head * head_dim), 
-                                                            k.view(batch, -1, self.tp_k_head_num_ * head_dim), 
-                                                            v.view(batch, -1, self.tp_v_head_num_ * head_dim), 
-                                                            seqlen, num_head, head_dim)
+        out = self.opt_ascend_prompt_attention_kernel(q.view(batch, -1, num_head * head_dim),
+                                                      k.view(batch, -1, self.tp_k_head_num_ * head_dim),
+                                                      v.view(batch, -1, self.tp_v_head_num_ * head_dim),
+                                                      seqlen, num_head, head_dim, num_key_value_heads)
 
         out = out.view(-1, self.tp_q_head_num_ * self.head_dim_)
-
         out = self.opt_context_post_process(input_embding, out, layer_weight, default_pg)
 
         return out
@@ -213,10 +206,8 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         cache_v = cache_v.view(-1, self.tp_v_head_num_, self.head_dim_)
 
         # post cache_kv
-        start_index = infer_state.int_index_list[0]
-        end_index = infer_state.int_index_list[0] + cache_k.shape[0]
-        infer_state.mem_manager.key_buffer[self.layer_num_][start_index:end_index] = cache_k
-        infer_state.mem_manager.value_buffer[self.layer_num_][start_index:end_index] = cache_v
+        infer_state.mem_manager.key_buffer[self.layer_num_][infer_state.kv_start_indices] = cache_k
+        infer_state.mem_manager.value_buffer[self.layer_num_][infer_state.kv_start_indices] = cache_v
 
         return q
     
@@ -249,16 +240,8 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
 
         return input_embding
 
-    def full_token_attention(self, input_embding, infer_state: LlamaInferStateInfo, layer_weight, current_len, max_seq_length, default_pg=None):
-        q = self.opt_token_pre_process(
-            input_embding, infer_state, layer_weight)
-        # o = self.opt_ascend_incre_attention_kernel(q.view(-1, self.tp_q_head_num_, self.head_dim_),
-        #                                            infer_state.mem_manager.key_buffer[self.layer_num_],
-        #                                            infer_state.mem_manager.value_buffer[self.layer_num_],
-        #                                            infer_state.int_index_list_t,
-        #                                            max_seq_length)  # max_seq_length 5 * 1024
-        # print('####')
-        # import pdb;pdb.set_trace()
+    def full_token_attention(self, input_embding, infer_state: LlamaInferStateInfo, layer_weight, max_seq_length, default_pg=None):
+        q = self.opt_token_pre_process(input_embding, infer_state, layer_weight)
         o = torch.ops.lightllm.paged_attention_inference.default(q.view(-1, self.tp_q_head_num_ * self.head_dim_),
                                                    infer_state.mem_manager.key_buffer[self.layer_num_].view(-1, self.tp_k_head_num_ * self.head_dim_),
                                                    infer_state.mem_manager.value_buffer[self.layer_num_].view(-1, self.tp_v_head_num_ * self.head_dim_),
@@ -268,6 +251,5 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
                                                    infer_state.block_table,
                                                    infer_state.b_seq_len.to(torch.int64),
                                                    PagingRequestManager.BLOCK_SIZE)
-        out = self.opt_token_post_process(
-            o, input_embding, infer_state, layer_weight, default_pg)
+        out = self.opt_token_post_process(o, input_embding, infer_state, layer_weight, default_pg)
         return out